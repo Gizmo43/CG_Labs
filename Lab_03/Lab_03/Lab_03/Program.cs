d724dc0b18231ecf5ff6cc646e477b46e7f69e4c
Gizmo43 <egarus2005@gmail.com>
2025-05-18 22:22:46 +0300

    first push

---
+++ Lab_03/Lab_03/Lab_03/Program.cs
@@ -0,0 +1,14 @@
+﻿using Lab_03;
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        using (RTX rtx = new RTX(600, 600))
+        {
+            rtx.Run();
+        }
+    }
+}
+
---
+++ Lab_03/Lab_03/Lab_03/RTX.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenTK.Audio.OpenAL;
+using OpenTK.Graphics.OpenGL4;
+using OpenTK.Mathematics;
+using OpenTK.Windowing.Common;
+using OpenTK.Windowing.Desktop;
+using OpenTK.Windowing.GraphicsLibraryFramework;
+using StbImageSharp;
+using Lab_03;
+
+
+namespace Lab_03
+{
+    class RTX : GameWindow
+    {
+        int width, height;
+        
+        int VAO;
+        int VBO;
+        int EBO;
+        Shader shaderProgram;
+        Vector3[] vertdata = new Vector3[] {
+            new Vector3(-1f, -1f, 0f),
+            new Vector3( 1f, -1f, 0f),
+            new Vector3( 1f, 1f, 0f),
+            new Vector3(-1f, 1f, 0f)
+        };
+        uint[] indices =
+        {
+            0, 1, 2,	 // front face
+			2, 3, 0
+        };
+        public RTX(int width, int height) : base
+        (GameWindowSettings.Default, NativeWindowSettings.Default)
+        {
+            this.CenterWindow(new Vector2i(width, height));
+            this.height = height;
+            this.width = width;
+        }
+        protected override void OnLoad()
+        {
+            VAO = GL.GenVertexArray(); //Create VAO
+            VBO = GL.GenBuffer(); //Create VBO
+            EBO = GL.GenBuffer();
+            GL.BindBuffer(BufferTarget.ArrayBuffer, VBO); //Bind the VBO
+            GL.BufferData(BufferTarget.ArrayBuffer, vertdata.Length * Vector3.SizeInBytes * //Copy vertices data to the buffer
+            sizeof(float), vertdata.ToArray(), BufferUsageHint.StaticDraw);
+            GL.BindVertexArray(VAO); //Bind the VAO
+            GL.VertexAttribPointer(0, 3, VertexAttribPointerType.Float, false, 0, 0); //Bind a slot number 0
+            GL.EnableVertexArrayAttrib(VAO, 0); //Enable the slot
+            GL.BindBuffer(BufferTarget.ArrayBuffer, 0); //Unbind the VBO
+
+            GL.BindBuffer(BufferTarget.ElementArrayBuffer, EBO);
+            GL.BufferData(BufferTarget.ElementArrayBuffer, indices.Length *
+            sizeof(uint), indices, BufferUsageHint.StaticDraw);
+            GL.BindBuffer(BufferTarget.ElementArrayBuffer, 0);  
+
+            shaderProgram = new Shader();
+            shaderProgram.LoadShader();
+
+            GL.BindVertexArray(0);
+            GL.Enable(EnableCap.DepthTest);
+
+            base.OnLoad();
+        }
+        protected override void OnUnload()
+        {
+            GL.DeleteBuffer(VAO);
+            GL.DeleteBuffer(VBO);
+            GL.DeleteBuffer(EBO);
+            shaderProgram.DeleteShader();
+            base.OnUnload();
+        }
+
+
+        protected override void OnRenderFrame(FrameEventArgs args)
+        {
+            GL.ClearColor(0.7f, 0.8f, 0.9f, 1f);
+            GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
+
+            GL.UseProgram(shaderProgram.shaderHandle);
+            GL.BindVertexArray(VAO);
+
+
+            GL.BindBuffer(BufferTarget.ElementArrayBuffer, EBO);
+            GL.DrawElements(PrimitiveType.Triangles, indices.Length,
+            DrawElementsType.UnsignedInt, 0);
+
+            Context.SwapBuffers();
+            base.OnRenderFrame(args);
+        }
+
+        protected override void OnUpdateFrame(FrameEventArgs args)
+        {
+            if (KeyboardState.IsKeyDown(Keys.Escape))
+            {
+                Close();
+            }
+            base.OnUpdateFrame(args);
+        }
+        protected override void OnResize(ResizeEventArgs e)
+        {
+            base.OnResize(e);
+            GL.Viewport(0, 0, e.Width, e.Height);
+            this.width = e.Width;
+            this.height = e.Height;
+        }
+    }
+
+
+    public class Shader
+    {
+        public int shaderHandle;
+
+        public void LoadShader()
+        {
+            shaderHandle = GL.CreateProgram();
+            int vertexShader = GL.CreateShader(ShaderType.VertexShader);
+            GL.ShaderSource(vertexShader, LoadShaderSource("shader.vert"));
+
+            GL.CompileShader(vertexShader);
+            GL.GetShader(vertexShader, ShaderParameter.CompileStatus, out int success1);
+            if (success1 == 0)
+            {
+                string infoLog = GL.GetShaderInfoLog(vertexShader);
+                Console.WriteLine(infoLog);
+            }
+
+            int fragmentShader = GL.CreateShader(ShaderType.FragmentShader);
+            GL.ShaderSource(fragmentShader, LoadShaderSource("shader.frag"));
+
+            GL.CompileShader(fragmentShader);
+            GL.GetShader(fragmentShader, ShaderParameter.CompileStatus, out int success2);
+            if (success2 == 0)
+            {
+                string infoLog = GL.GetShaderInfoLog(fragmentShader);
+                Console.WriteLine(infoLog);
+            }
+
+            GL.AttachShader(shaderHandle, vertexShader);
+            GL.AttachShader(shaderHandle, fragmentShader);
+            GL.LinkProgram(shaderHandle);
+        }
+        public static string LoadShaderSource(string filepath)
+        {
+            string shaderSource = "";
+            try
+            {
+                using (StreamReader reader = new
+                StreamReader("../../../Shaders/" + filepath))
+                {
+                    shaderSource = reader.ReadToEnd();
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Failed to load shader source file:" + e.Message);
+            }
+            return shaderSource;
+        }
+
+        public void UseShader()
+        {
+            GL.UseProgram(shaderHandle);
+        }
+        public void DeleteShader()
+        {
+            GL.DeleteProgram(shaderHandle);
+        }
+    }
+
+
+}
